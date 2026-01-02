1c6c0c074393abe7c81075aaac5c0172dcf028ba
bargavanr <bargavanroboticsengineer@gmail.com>
2026-01-02 12:04:05 +0530

    setup changes

--- servo_service/setup.py
+++ servo_service/setup.py
@@ -1,32 +1,29 @@
-from setuptools import find_packages, setup
+from setuptools import setup, find_packages
+from glob import glob
+import os
 
 package_name = 'servo_service'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=find_packages(),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/srv', ['srv/SetServoAngle.srv']),
-
+        (os.path.join('share', package_name, 'srv'),
+            glob('srv/*.srv')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='bargavan',
     maintainer_email='bargavanroboticsengineer@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
+    description='Servo control via ROS 2 service using Jetson GPIO PWM',
+    license='Apache License 2.0',
     entry_points={
         'console_scripts': [
-            'servo_node = servo_service.servo_node:main'
+            'servo_node = servo_service.servo_node:main',
         ],
     },
 )
